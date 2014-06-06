3c1d522ecd4b99fc27b45260ed37112d054e90f8
Ian MacLarty <ian@ianmaclarty.com>
2014-06-07 09:26:15 +1000

    align trail entries

--- src/trail.cpp
+++ src/trail.cpp
@@ -2,7 +2,8 @@
 
 struct am_trail_entry {
     void *ptr;
-    int size;
+    int value_size;
+    int entry_size;
     int prev;
     char value[];
 };
@@ -26,27 +27,30 @@ void am_trail::rewind_to(int ticket) {
     while (end != ticket) {
         assert(end > ticket);
         am_trail_entry *entry = (am_trail_entry*)(base + last);
-        memcpy(entry->ptr, &entry->value[0], entry->size);
-        end -= entry->size + sizeof(am_trail_entry);
+        memcpy(entry->ptr, &entry->value[0], entry->value_size);
+        end = last;
         last = entry->prev;
     }
 }
 
+#define ALIGN_MASK (sizeof(void*) - 1)
+
 void am_trail::trail(void *ptr, int size) {
-    ensure_big_enough(size);
+    int entry_size = sizeof(am_trail_entry) + size;
+    while (entry_size & ALIGN_MASK) entry_size++;
+
+    int required_capacity = end + entry_size;
+    if (required_capacity > capacity) {
+        while (required_capacity > capacity) capacity *= 2;
+        base = (char*)realloc(base, capacity);
+    }
+
     am_trail_entry *entry = (am_trail_entry*)(base + end);
     entry->ptr = ptr;
-    entry->size = size;
+    entry->value_size = size;
+    entry->entry_size = entry_size;
     entry->prev = last;
     memcpy(&entry->value[0], ptr, size);
     last = end;
-    end += sizeof(am_trail_entry) + size;
-}
-
-void am_trail::ensure_big_enough(int size) {
-    int required_size = end + sizeof(am_trail_entry) + size;
-    if (required_size > capacity) {
-        while (required_size > capacity) capacity *= 2;
-        base = (char*)realloc(base, capacity);
-    }
+    end += entry_size;
 }
--- src/trail.h
+++ src/trail.h
@@ -10,6 +10,4 @@ struct am_trail {
     int get_ticket();
     void rewind_to(int ticket);
     void trail(void *ptr, int size);
-
-    void ensure_big_enough(int size);
 };
