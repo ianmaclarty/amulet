931d0c6f7830bd217e92d7838989c07ace0b32db
Ian MacLarty <ian@ianmaclarty.com>
2015-05-02 22:11:24 +1000

    add math.sphere_visible

---
+++ samples/culling.lua
@@ -0,0 +1,85 @@
+local am = amulet
+local win = am.window{depth_buffer = true}
+
+local vshader = [[
+precision mediump float;
+attribute vec3 pos;
+attribute vec3 normal;
+uniform mat4 M;
+uniform mat4 P;
+varying vec3 v_color;
+void main() {
+    v_color = normal * 0.5 + 0.5;
+    gl_Position = P * M * vec4(pos, 1.0);
+}
+]]
+
+local fshader = [[
+precision mediump float;
+varying vec3 v_color;
+void main() {
+    gl_FragColor = vec4(v_color, 1.0);
+}
+]]
+
+local shader = am.program(vshader, fshader)
+
+local
+function load_model(name)
+    local buf, stride, normals_offset, tex_offset = am.load_obj(name)
+    local verts = buf:view("vec3", 0, stride)
+    local normals = buf:view("vec3", normals_offset, stride)
+    return am.draw_arrays("triangles")
+        :bind_array("pos", verts)
+        :bind_array("normal", normals)
+end
+
+local cube = load_model("cube.obj")
+
+local modelview_matrix = mat4(1)
+
+local rotating_cube = cube
+    :read_mat4("M")
+    :action(function(node)
+        modelview_matrix = node.value
+        --log(modelview_matrix)
+        return 0
+    end)
+    --[[
+    :rotate("M", 0, 0, 0, -1)
+    :action(function(node)
+        node.angle = 1 * am.frame_time
+        return 0
+    end)
+    :rotate("M", 0, 0, 1, 0)
+    :action(function(node)
+        node.angle = 1.7 * am.frame_time
+        return 0
+    end)
+    :rotate("M", 0, 1, 0, 0)
+    :action(function(node)
+        node.angle = 3.2 * am.frame_time
+        return 0
+    end)]]
+local scene_group = am.group()
+
+local translated_cube = rotating_cube:translate("M", 0, 0, -7)
+
+local projection_matrix = math.perspective(math.rad(85), 1, 0.1, 10)
+
+scene_group:append(translated_cube)
+
+local scene = scene_group
+    :bind_mat4("M", mat4(1))
+    :bind_mat4("P", projection_matrix)
+    :bind_program(shader)
+    :action(function()
+        translated_cube.xy = am.mouse_position.xy * 10
+        log(math.sphere_visible(projection_matrix * modelview_matrix, vec3(0), math.sqrt(3)))
+        if am.key_pressed.escape then
+            win:close()
+        end
+        return 0
+    end)
+
+win.root = scene
--- src/am_math.cpp
+++ src/am_math.cpp
@@ -1063,6 +1063,59 @@ static int euleryxz4(lua_State *L) {
     return 1;
 }
 
+inline static glm::vec4 plain_normalize(glm::vec4 plain) {
+    float l = glm::length(glm::vec3(plain.x, plain.y, plain.z));
+    return glm::vec4(plain.x/l, plain.y/l, plain.z/l, plain.w/l);
+}
+
+static int sphere_visible(lua_State *L) {
+    int nargs = am_check_nargs(L, 2);
+    glm::mat4 matrix = am_get_userdata(L, am_mat4, 1)->m;
+    glm::vec4 center = glm::vec4(am_get_userdata(L, am_vec3, 2)->v, 1.0f);
+    float radius = 0.0f;
+    if (nargs > 2) {
+        radius = luaL_checknumber(L, 3);
+    }
+    glm::vec4 left_plane = plain_normalize(glm::row(matrix, 0) + glm::row(matrix, 3));
+    float left_dist = glm::dot(left_plane, center);
+    if (left_dist + radius < 0.0f) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    glm::vec4 right_plane = plain_normalize(glm::row(-matrix, 0) + glm::row(matrix, 3));
+    float right_dist = glm::dot(right_plane, center);
+    if (right_dist + radius < 0.0f) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    glm::vec4 bottom_plane = plain_normalize(glm::row(matrix, 1) + glm::row(matrix, 3));
+    float bottom_dist = glm::dot(bottom_plane, center);
+    if (bottom_dist + radius < 0.0f) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    glm::vec4 top_plane = plain_normalize(glm::row(-matrix, 1) + glm::row(matrix, 3));
+    float top_dist = glm::dot(top_plane, center);
+    if (top_dist + radius < 0.0f) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    glm::vec4 near_plane = plain_normalize(glm::row(matrix, 2) + glm::row(matrix, 3));
+    float near_dist = glm::dot(near_plane, center);
+    if (near_dist + radius < 0.0f) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    glm::vec4 far_plane = plain_normalize(glm::row(-matrix, 2) + glm::row(matrix, 3));
+    float far_dist = glm::dot(far_plane, center);
+    if (far_dist + radius < 0.0f) {
+        lua_pushboolean(L, 0);
+        return 1;
+    }
+    lua_pushboolean(L, 1);
+    return 1;
+}
+
 static int inverse(lua_State *L) {
     am_check_nargs(L, 1);
     switch (am_get_type(L, 1)) {
@@ -1568,6 +1621,7 @@ void am_open_math_module(lua_State *L) {
         {"lookat",      lookat},
         {"euleryxz3",   euleryxz3},
         {"euleryxz4",   euleryxz4},
+        {"sphere_visible", sphere_visible},
         {"inverse",     inverse},
         {"simplex",     simplex},
         {"perlin",      perlin},
