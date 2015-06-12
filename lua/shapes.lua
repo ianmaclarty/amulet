e83fcfad4ed2cba35da7db0570f0f1dd0c11dfdc
Ian MacLarty <ian@ianmaclarty.com>
2015-06-12 16:13:28 +1000

    add rect vert funcs

--- lua/shapes.lua
+++ lua/shapes.lua
@@ -1,10 +1,16 @@
-function amulet.rect(x1, y1, x2, y2)
-    local pos_view = amulet.buffer(8*4):view("vec2", 0, 8)
-    pos_view:set_nums{
-        x1, y1,
-        x2, y1,
-        x2, y2,
-        x1, y2,
-    }
-    return amulet.draw_arrays("triangle_fan"):bind_array("pos", pos_view)
+-- rects:
+-- 1-4
+-- |\|
+-- 2-3
+
+function amulet.rect_indices() 
+    return amulet.ushort_elem_array{1, 2, 3, 1, 3, 4}
+end
+
+function amulet.rect_verts_2d(x1, y1, x2, y2)
+    return amulet.vec2_array{x1, y2, x1, y1, x2, y1, x2, y2}
+end
+
+function amulet.rect_verts_3d(x1, y1, x2, y2)
+    return amulet.vec3_array{x1, y2, 0, x1, y1, 0, x2, y1, 0, x2, y2, 0}
 end
