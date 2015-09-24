f19df1cdc038dff76fc3eda9eb35c28fc26a1aff
Ian MacLarty <ian.maclarty@gmail.com>
2015-09-24 17:14:44 +1000

    travis: try html build

--- travis-build.sh
+++ travis-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 make clean-all
-cp travis-linux/settings.mingw32 settings && make
+cp travis-linux/settings.html settings && make
 #for s in `ls travis-$TRAVIS_OS_NAME/settings.*`; do
 #    cp $s settings && make
 #done
