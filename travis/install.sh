57a3164b47c70e136df7a23eee89dcc3d54a0d05
Ian MacLarty <ian.maclarty@gmail.com>
2015-09-24 11:15:50 +1000

    move sudo to .travis.yml for install script

--- travis/install.sh
+++ travis/install.sh
@@ -2,10 +2,10 @@
 set -ev
 dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo apt-get update -qq
-    sudo apt-get install -y libc6-dev-i386 g++-multilib
-    sudo apt-get install -y libasound2-dev
-    sudo apt-get install -y libasound2-dev:i386
-    sudo apt-get install -y mingw-w64
-    sudo apt-get install -y python2.7 nodejs default-jre
+    apt-get update -qq
+    apt-get install -y libc6-dev-i386 g++-multilib
+    apt-get install -y libasound2-dev
+    apt-get install -y libasound2-dev:i386
+    apt-get install -y mingw-w64
+    apt-get install -y python2.7 nodejs default-jre
 fi
