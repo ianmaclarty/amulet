a3a51fc73414bc1738f2acf3a3c7af19d2648cd6
Ian MacLarty <ian.maclarty@gmail.com>
2015-10-27 15:56:35 +1100

    appveyor: try emmake

--- appveyor.bat
+++ appveyor.bat
@@ -1,4 +1,6 @@
-call "%VS140COMNTOOLS%vsvars32.bat"
-SET PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+SET PATH=C:\emscripten
 call "C:\emscripten\emsdk_env.bat"
-make TARGET=html.release
+emmake TARGET=html.release
+call "%VS140COMNTOOLS%vsvars32.bat"
+SET PATH=C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+make
