263428ae859ed1eff2180f5422e19dd5f9ad174d
Ian MacLarty <ian.maclarty@gmail.com>
2015-10-27 14:04:39 +1100

    appveyor: add emscripten path

--- appveyor.bat
+++ appveyor.bat
@@ -1,3 +1,4 @@
+DIR C:\emscripten
 call "%VS140COMNTOOLS%vsvars32.bat"
-SET PATH=C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+SET PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 make TARGET=html.release
