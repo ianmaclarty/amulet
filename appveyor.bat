945607dfa22811ba0c11054f8884abc2925e6106
Ian MacLarty <ian.maclarty@gmail.com>
2015-10-27 14:29:06 +1100

    appveyor: more emscripten fixes

--- appveyor.bat
+++ appveyor.bat
@@ -1,4 +1,4 @@
 call "%VS140COMNTOOLS%vsvars32.bat"
+SET PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 call "C:\emscripten\emsdk_env.bat"
-SET PATH=C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
 make TARGET=html.release
