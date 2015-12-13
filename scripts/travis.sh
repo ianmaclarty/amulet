50391012105f050296547a0ba9b40a123ce67c73
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-13 17:57:11 +1100

    travis: build all

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -2,14 +2,14 @@
 set -e
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     make TARGET=linux64.release LUAVM=lua51   test
-    #make TARGET=linux64.release LUAVM=lua52   test
-    #make TARGET=linux64.release LUAVM=luajit  test
-    #make TARGET=mingw32.release
+    make TARGET=linux64.release LUAVM=lua52   test
+    make TARGET=linux64.release LUAVM=luajit  test
+    make TARGET=mingw32.release
 else
     make TARGET=osx.release     LUAVM=lua51   test
-    #make TARGET=osx.release     LUAVM=luajit  test
-    #make TARGET=ios32.release   LUAVM=lua51
-    #make TARGET=ios64.release   LUAVM=lua51
+    make TARGET=osx.release     LUAVM=luajit  test
+    make TARGET=ios32.release   LUAVM=lua51
+    make TARGET=ios64.release   LUAVM=lua51
 fi
 if [ -n "$TRAVIS_TAG" ]; then
     scripts/upload_release.js $TRAVIS_TAG
