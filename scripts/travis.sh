ae02a673ad9d2852bd19fb25a345716fd7e05b4b
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-13 17:44:17 +1100

    travis fix script bug

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,11 +5,13 @@ if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     #make TARGET=linux64.release LUAVM=lua52   test
     #make TARGET=linux64.release LUAVM=luajit  test
     #make TARGET=mingw32.release
+    echo test
 else
     #make TARGET=osx.release     LUAVM=lua51   test
     #make TARGET=osx.release     LUAVM=luajit  test
     #make TARGET=ios32.release   LUAVM=lua51
     #make TARGET=ios64.release   LUAVM=lua51
+    echo test
 fi
 echo TRAVIS_TAG: $TRAVIS_TAG
 if [ -n "$TRAVIS_TAG" ]; then
