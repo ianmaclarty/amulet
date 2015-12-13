2cb2895f4bf2fee0fa80f2365d298263f9b473ef
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-13 17:50:04 +1100

    travis: test tag deploy with one build

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 set -e
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    #make TARGET=linux64.release LUAVM=lua51   test
+    make TARGET=linux64.release LUAVM=lua51   test
     #make TARGET=linux64.release LUAVM=lua52   test
     #make TARGET=linux64.release LUAVM=luajit  test
     #make TARGET=mingw32.release
-    echo test
 else
-    #make TARGET=osx.release     LUAVM=lua51   test
+    make TARGET=osx.release     LUAVM=lua51   test
     #make TARGET=osx.release     LUAVM=luajit  test
     #make TARGET=ios32.release   LUAVM=lua51
     #make TARGET=ios64.release   LUAVM=lua51
-    echo test
 fi
-echo TRAVIS_TAG: $TRAVIS_TAG
 if [ -n "$TRAVIS_TAG" ]; then
     scripts/upload_release.js $TRAVIS_TAG
 fi
