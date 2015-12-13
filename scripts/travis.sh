cf2405452896a960849b1ffc4d2118250008531d
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-13 17:41:11 +1100

    avoid travis warning

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    make TARGET=linux64.release LUAVM=lua51   test
+    #make TARGET=linux64.release LUAVM=lua51   test
     #make TARGET=linux64.release LUAVM=lua52   test
     #make TARGET=linux64.release LUAVM=luajit  test
     #make TARGET=mingw32.release
 else
-    make TARGET=osx.release     LUAVM=lua51   test
+    #make TARGET=osx.release     LUAVM=lua51   test
     #make TARGET=osx.release     LUAVM=luajit  test
     #make TARGET=ios32.release   LUAVM=lua51
     #make TARGET=ios64.release   LUAVM=lua51
 fi
+echo TRAVIS_TAG: $TRAVIS_TAG
 if [ -n "$TRAVIS_TAG" ]; then
     scripts/upload_release.js $TRAVIS_TAG
 fi
