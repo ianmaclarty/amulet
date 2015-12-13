5faa62ae6682dc233830b6e3b04a7ec5cc1b36c2
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-13 22:52:43 +1100

    appveyor: restore full build

--- scripts/appveyor.bat
+++ scripts/appveyor.bat
@@ -1,8 +1,8 @@
 call "%VS140COMNTOOLS%vsvars32.bat"
 SET PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
-REM call "C:\emscripten\emsdk_env.bat"
-REM make TARGET=html.release
-REM if %errorlevel% neq 0 exit /b %errorlevel%
+call "C:\emscripten\emsdk_env.bat"
+make TARGET=html.release
+if %errorlevel% neq 0 exit /b %errorlevel%
 make TARGET=msvc32.release
 if %errorlevel% neq 0 exit /b %errorlevel%
 if defined APPVEYOR_REPO_TAG_NAME (node scripts\upload_release.js %APPVEYOR_REPO_TAG_NAME%)
