09e3a5668c3ed6f56d9679609c0d39a0fe8bfc74
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-16 09:40:00 +1100

    appveyor: fiddling

--- scripts/appveyor.bat
+++ scripts/appveyor.bat
@@ -1,14 +1,9 @@
-if "%APPVEYOR_REPO_TAG_NAME:~-15%" neq "-distro-trigger" (
-    call "%VS140COMNTOOLS%vsvars32.bat"
-    SET PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
-    REM call "C:\emscripten\emsdk_env.bat"
-    REM make TARGET=html.release
-    REM if %errorlevel% neq 0 exit /b %errorlevel%
-    make TARGET=msvc32.release
-    if %errorlevel% neq 0 exit /b %errorlevel%
-    if defined APPVEYOR_REPO_TAG_NAME (node scripts\upload_builds.js %APPVEYOR_REPO_TAG_NAME%)
-    if %errorlevel% neq 0 exit /b %errorlevel%
-) else (
-    node scripts\build_distro.js %APPVEYOR_REPO_TAG_NAME%
-    if %errorlevel% neq 0 exit /b %errorlevel%
-)
+call "%VS140COMNTOOLS%vsvars32.bat"
+SET PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+REM call "C:\emscripten\emsdk_env.bat"
+REM make TARGET=html.release
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+make TARGET=msvc32.release
+if %errorlevel% neq 0 exit /b %errorlevel%
+if defined APPVEYOR_REPO_TAG_NAME (node scripts\upload_builds.js %APPVEYOR_REPO_TAG_NAME%)
+if %errorlevel% neq 0 exit /b %errorlevel%
