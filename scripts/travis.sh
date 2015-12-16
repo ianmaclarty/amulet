3fa5d65ccc1a42c7bd0f3a475f8d8d882fa12766
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-16 12:23:31 +1100

    travis: wrong build

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -19,9 +19,9 @@ if [[ "$TRAVIS_TAG" == *-distro-trigger ]]; then
     scripts/upload_distros.js $TAG amulet-${TAG}-${TRAVIS_OS_NAME}.zip
 else
     if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-        #make TARGET=linux64.release LUAVM=lua51   test
+        make TARGET=linux64.release LUAVM=lua51   test
         #make TARGET=linux64.release LUAVM=lua52   test
-        make TARGET=linux64.release LUAVM=luajit  test
+        #make TARGET=linux64.release LUAVM=luajit  test
         #make TARGET=mingw32.release
     else
         make TARGET=osx.release     LUAVM=lua51   test
