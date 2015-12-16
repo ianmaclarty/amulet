8133f853fe6b97ab394a04a817808695cfe2bb3e
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-16 13:49:57 +1100

    distro build: clean up

--- scripts/appveyor.bat
+++ scripts/appveyor.bat
@@ -1,11 +1,13 @@
 call "%VS140COMNTOOLS%vsvars32.bat"
-set PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;%PATH%
+set PATH=C:\emscripten;C:\MinGW\bin;C:\MinGW\msys\1.0\bin;"C:\Program Files (x86)\Inno Setup 5";%PATH%
 
 if "%APPVEYOR_REPO_TAG_NAME:~-15%" == "-distro-trigger" goto builddistro
 
-REM call "C:\emscripten\emsdk_env.bat"
-REM make TARGET=html.release
-REM if %errorlevel% neq 0 exit /b %errorlevel%
+curl -L -O https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-1.34.1-full-64bit.exe
+emsdk-1.34.1-full-64bit.exe /S /D=C:\emscripten
+call "C:\emscripten\emsdk_env.bat"
+make TARGET=html.release
+if %errorlevel% neq 0 exit /b %errorlevel%
 make TARGET=msvc32.release
 if %errorlevel% neq 0 exit /b %errorlevel%
 if defined APPVEYOR_REPO_TAG_NAME (node scripts\upload_builds.js %APPVEYOR_REPO_TAG_NAME%)
@@ -15,36 +17,22 @@ goto end
 :builddistro
 set TAG=%APPVEYOR_REPO_TAG_NAME:~0,-15%
 choco install -y InnoSetup
-set PATH=%PATH%;"C:\Program Files (x86)\Inno Setup 5"
-mingw-get install zip
-echo step1
 curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-darwin.zip
 curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-win32.zip
 curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-linux.zip
-echo step1b
 unzip builds-darwin.zip
 unzip builds-win32.zip
 unzip builds-linux.zip
-echo step2
 mkdir scripts\installer-payload
-echo step3
 cp -r builds scripts/installer-payload/
-echo step4
 cp -r builds/msvc/lua51/release/bin/* scripts/installer-payload/
-echo step5
 cp scripts/icon.ico scripts/installer-payload/
-echo step6
 iscc /DVERSION=%TAG% scripts\installer.iss
 if %errorlevel% neq 0 exit /b %errorlevel%
-echo step7
 cp scripts/output/setup.exe amulet-%TAG%-windows.exe
-echo step8
 mv scripts/installer-payload amulet-%TAG%
-echo step9
 zip -r amulet-%TAG%-windows.zip amulet-%TAG%
-echo step10
 node scripts\upload_distros.js %TAG% amulet-%TAG%-windows.exe amulet-%TAG%-windows.zip
 if %errorlevel% neq 0 exit /b %errorlevel%
-echo step11
 
 :end
