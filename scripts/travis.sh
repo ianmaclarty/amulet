844873c75bec3cadf80d2b6d0213161c6e13e874
Ian MacLarty <ian@ianmaclarty.com>
2015-12-30 23:36:08 +1100

    build linux32

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -29,12 +29,13 @@ if [[ "$TRAVIS_TAG" == *-distro-trigger ]]; then
 else
     if [ "$TRAVIS_OS_NAME" = "linux" ]; then
         make TARGET=linux64.release LUAVM=lua51   test
-        make TARGET=linux64.release LUAVM=lua52   test
-        make TARGET=linux64.release LUAVM=luajit  test
+        #make TARGET=linux64.release LUAVM=lua52   test
+        #make TARGET=linux64.release LUAVM=luajit  test
+        make TARGET=linux32.release LUAVM=lua51   test
         make TARGET=mingw32.release
     else
         make TARGET=osx.release     LUAVM=lua51   test
-        make TARGET=osx.release     LUAVM=luajit  test
+        #make TARGET=osx.release     LUAVM=luajit  test
         make TARGET=ios32.release   LUAVM=lua51
         make TARGET=ios64.release   LUAVM=lua51
     fi
