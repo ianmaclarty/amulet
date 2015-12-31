9f38d719e7373794b1ad6b715be8a5e75dd4ffa9
Ian MacLarty <ian.maclarty@gmail.com>
2015-12-31 16:52:24 +1100

    fix undefined var error

--- scripts/upload_builds.js
+++ scripts/upload_builds.js
@@ -97,7 +97,7 @@ function upload_asset(upload_url, data, name, type, cb) {
         if (Math.floor(code / 100) == 2) {
             cb();
         } else {
-            throw "unable to upload file " + file + ": " + JSON.stringify(resp);
+            throw "unable to upload file " + name + ": " + code + ": " + JSON.stringify(resp);
         }
     });
 }
