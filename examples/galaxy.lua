7daa304dc90214bd82c212727a7da350a93f5908
Ian MacLarty <ian.maclarty@gmail.com>
2016-01-07 01:02:45 +1100

    hello galaxy updates

--- examples/galaxy.lua
+++ examples/galaxy.lua
@@ -13,34 +13,28 @@ local laser_velocity = vec2(0, 600)
 local laser_interval = 0.1
 
 local ship_sprite = am.sprite[[
-.........
-....W....
-.R.WWW.R.
-.W.WBW.W.
-.WWWBWWW.
-...WWW...
-.........
+...W...
+R.WWW.R
+W.WBW.W
+WWWBWWW
+..WWW..
 ]]
 
 local laser_sprite = am.sprite[[
-.........
-..YYYYY..
-.YOOOOOY.
-.ORRRRRO.
-.R.....R.
-.........
-.R.....R.
-.........
+.YYYYY.
+YOOOOOY
+ORRRRRO
+R.....R
+.......
+R.....R
 ]]
 
 local bullet_sprite = am.sprite[[
-.......
-..BBB..
-.BBCBB.
-.BCWCB.
-.BBCBB.
-..BBB..
-.......
+.mmm.
+mmMmm
+mMWMm
+mmMmm
+.mmm.
 ]]
 
 local star_img = [[
@@ -50,21 +44,53 @@ local star_img = [[
 
 local laser_buf = am.sfxr_synth(86572901)
 local explode_buf = am.sfxr_synth(9975402)
+local ship_explode_buf = am.sfxr_synth(20755502)
+local hit_buf = am.sfxr_synth(14120204)
 
 local spawn_bullet
 
+local
+function animated_sprite(frames, delay)
+    local frame = 1
+    local t = am.frame_time
+    return am.sprite(frames[1]):action(function(sprite)
+        if am.frame_time - t >= delay then
+            frame = frame % #frames + 1
+            sprite.source = frames[frame]
+            t = am.frame_time
+        end
+    end)
+end
+
 -- slug enemy
 
-local slug_sprite = am.sprite[[
+local slug_frame1 = [[
 GGGGGGGG
 GggggggG
 GgGGGGgG
-GgGBBGgG
-GgGBBGgG
+GgGMMGgG
+GgGMMGgG
 GgGGGGgG
 GggggggG
 GGGGGGGG
 ]]
+
+local slug_frame2 = [[
+gggggggg
+gGGGGGGg
+gGggggGg
+gGgmmgGg
+gGgmmgGg
+gGggggGg
+gGGGGGGg
+gggggggg
+]]
+
+local slug_hit_frame = slug_frame1:gsub("[^%.%s]", "W")
+
+local slug_sprite = animated_sprite({slug_frame1, slug_frame2}, 0.5)
+
+local slug_hit_sprite = am.sprite(slug_hit_frame)
 
 local
 function slug_update(enemy, state)
@@ -84,7 +110,21 @@ function slug_update(enemy, state)
 end
 
 local
-function slug(position)
+function slug_hit(enemy)
+    local node = enemy.node
+    node"scale":remove_all()
+    node"scale":append(slug_hit_sprite)
+    node:action(am.play(hit_buf))
+    node:action(am.series{am.delay(0.1), function()
+        node"scale":remove_all()
+        node"scale":append(slug_sprite)
+        return true
+    end})
+end
+
+local
+function slug(x)
+    local position = vec2(x, win.top + 25)
     local node = am.translate(position)
         ^ am.scale(4)
         ^ slug_sprite
@@ -94,50 +134,95 @@ function slug(position)
         position = position,
         last_fire_time = am.frame_time,
         hp = 1,
-        points = 100,
+        points = 200,
         t = am.frame_time,
         radius = 20,
         explosion_size = 1,
         explosion_color = vec4(0.1, 1, 0.1, 1),
         explosion_color_var = vec4(0.2, 0.2, 0.2, 0),
+        hit = slug_hit,
     }
 end
 
 -- wraith enemy
 
-local wraith_sprite = am.sprite[[
-....................
-...r................
-...r...rrrrrr..r....
-...rrrrrRRRRr.rr....
-...R...rRBBRrrrr....
-.......rRBBRrrRr....
-......rrrrr..r.r....
-.rrrrrR.r.r..rrRRrr.
-.Rr.rrr.r.r..r.rrrr.
-.r..r.r...r....r.rr.
-.R....r........r..R.
+local wraith_frame1 = [[
+...r.r.r...
+.rrrrrrrrr.
+.rRrrrrrRr.
+rrrrMMMrrrr
+.rrMMmMMrr.
+rrrMmmmMrrr
+.rrMMmMMrr.
+rrrrMMMrrrr
+.rRrrrrrRr.
+.rrrrrrrrr.
+...r.r.r...
 ]]
+
+local wraith_frame2 = [[
+...r.r.r...
+.rrrrrrrrr.
+.rrrrRrrrr.
+rrrrmmmrrrr
+.rrmmMmmrr.
+rrRmMMMmRrr
+.rrmmMmmrr.
+rrrrmmmrrrr
+.rrrrRrrrr.
+.rrrrrrrrr.
+...r.r.r...
+]]
+
+local wraith_hit_frame = wraith_frame1:gsub("[^%.%s]", "W")
+
+local wraith_sprite = animated_sprite({wraith_frame1, wraith_frame2}, 0.5)
+local wraith_hit_sprite = am.sprite(wraith_hit_frame)
 
 local
 function wraith_update(enemy, state)
     local dt = am.delta_time
     local pos = enemy.position
     local t = am.frame_time - enemy.t
-    local vx = math.sin(t*3) * 150
-    local vy = -80
+    local phase_time = 0.5
+    local phase = math.floor(t / phase_time) % 8
+    local vx, vy = 0, 0
+    if phase == 0 or phase == 4 then
+        vx = 0
+        vy = -160
+    elseif phase == 2 then
+        vx = 200
+        vy = 0
+    elseif phase == 6 then
+        vx = -200
+        vy = 0
+    end
     pos = pos + vec2(vx, vy) * dt
     enemy.position = pos
     enemy.node.position2d = pos
-    if am.frame_time > enemy.last_fire_time + 1 then
-        local vel = math.normalize(state.ship.position - pos) * 150
+    if am.frame_time > enemy.last_fire_time + phase_time * 2 then
+        local vel = math.normalize(state.ship.position - pos) * 200
         spawn_bullet(state, pos, vel)
         enemy.last_fire_time = am.frame_time
     end
 end
 
 local
-function wraith(position)
+function wraith_hit(enemy)
+    local node = enemy.node
+    node"scale":remove_all()
+    node"scale":append(wraith_hit_sprite)
+    node:action(am.play(hit_buf))
+    node:action(am.series{am.delay(0.1), function()
+        node"scale":remove_all()
+        node"scale":append(wraith_sprite)
+        return true
+    end})
+end
+
+local
+function wraith(x)
+    local position = vec2(x, win.top + 30)
     local node = am.translate(position)
         ^ am.scale(4)
         ^ wraith_sprite
@@ -146,50 +231,163 @@ function wraith(position)
         update = wraith_update,
         position = position,
         last_fire_time = am.frame_time,
-        hp = 3,
+        hp = 4,
         points = 500,
         t = am.frame_time,
         radius = 25,
         explosion_size = 1.5,
         explosion_color = vec4(1, 0.1, 0.1, 1),
         explosion_color_var = vec4(0.2, 0.2, 0.2, 0),
+        hit = wraith_hit,
     }
 end
 
+-- worm enemy
+
+local worm_frame1 = [[
+...b...
+..bBb..
+.bBCBb.
+bBCCCBb
+.bBCBb.
+..bBb..
+...b...
+]]
+
+local worm_frame2 = [[
+...C...
+..CBC..
+.CBbBC.
+CBbbbBC
+.CBbBC.
+..CBC..
+...C...
+]]
+
+local worm_hit_frame = worm_frame1:gsub("[^%.%s]", "W")
+
+local worm_sprite = animated_sprite({worm_frame1, worm_frame2}, 0.5)
+local worm_hit_sprite = am.sprite(worm_hit_frame)
+
+local
+function worm_update(enemy, state)
+    local dt = am.delta_time
+    local pos = enemy.position
+    local center = enemy.center
+    local dir = enemy.dir
+    local t = am.frame_time - enemy.t
+    local vx, vy = dir * 80, -80
+    center = center + vec2(vx, vy) * dt
+    local radius = 50
+    pos = center + vec2(math.cos(t*4*dir) * radius, math.sin(t*4*dir) * radius)
+    enemy.center = center
+    enemy.position = pos
+    enemy.node.position2d = pos
+end
+
+local
+function worm_hit(enemy)
+    local node = enemy.node
+    node"scale":remove_all()
+    node"scale":append(worm_hit_sprite)
+    node:action(am.play(hit_buf))
+    node:action(am.series{am.delay(0.1), function()
+        node"scale":remove_all()
+        node"scale":append(worm_sprite)
+        return true
+    end})
+end
+
+local
+function worm(x, dir)
+    local position = vec2(x, win.top+20)
+    local node = am.translate(position)
+        ^ am.scale(4)
+        ^ worm_sprite
+    return {
+        node = node,
+        dir = dir,
+        update = worm_update,
+        position = position,
+        center = position,
+        last_fire_time = am.frame_time,
+        hp = 1,
+        points = 100,
+        t = am.frame_time,
+        radius = 15,
+        explosion_size = 0.5,
+        explosion_color = vec4(0.2, 1, 1, 1),
+        explosion_color_var = vec4(0.2, 0.2, 0.2, 0),
+        hit = worm_hit,
+    }
+end
+
+----------------------
+
 local enemy_spawn_patterns = {
     {
-        {enemy = slug, position = vec2(-200, 340), delay = 1},
-        {enemy = slug, position = vec2(-200, 340), delay = 0.5},
-        {enemy = slug, position = vec2(-200, 340), delay = 0.5},
-        {enemy = slug, position = vec2(0, 340), delay = 1},
-        {enemy = slug, position = vec2(0, 340), delay = 0.5},
-        {enemy = slug, position = vec2(0, 340), delay = 0.5},
-        {enemy = slug, position = vec2(200, 340), delay = 1},
-        {enemy = slug, position = vec2(200, 340), delay = 0.5},
-        {enemy = slug, position = vec2(200, 340), delay = 0.5},
+        {enemy = slug, args = {-200}, delay = 1},
+        {enemy = slug, args = {-200}, delay = 0.4},
+        {enemy = slug, args = {-200}, delay = 0.4},
+        {enemy = slug, args = {-200}, delay = 0.4},
+        {enemy = slug, args = {0,  }, delay = 1},
+        {enemy = slug, args = {0,  }, delay = 0.4},
+        {enemy = slug, args = {0,  }, delay = 0.4},
+        {enemy = slug, args = {0,  }, delay = 0.4},
+        {enemy = slug, args = {200 }, delay = 1},
+        {enemy = slug, args = {200 }, delay = 0.4},
+        {enemy = slug, args = {200 }, delay = 0.4},
+        {enemy = slug, args = {200 }, delay = 0.4},
     },
     {
-        {enemy = slug, position = vec2(200, 340), delay = 1},
-        {enemy = slug, position = vec2(200, 340), delay = 0.5},
-        {enemy = slug, position = vec2(200, 340), delay = 0.5},
-        {enemy = slug, position = vec2(0, 340), delay = 1},
-        {enemy = slug, position = vec2(0, 340), delay = 0.5},
-        {enemy = slug, position = vec2(0, 340), delay = 0.5},
-        {enemy = slug, position = vec2(-200, 340), delay = 1},
-        {enemy = slug, position = vec2(-200, 340), delay = 0.5},
-        {enemy = slug, position = vec2(-200, 340), delay = 0.5},
+        {enemy = slug, args = {200},  delay = 1},
+        {enemy = slug, args = {200},  delay = 0.4},
+        {enemy = slug, args = {200},  delay = 0.4},
+        {enemy = slug, args = {200},  delay = 0.4},
+        {enemy = slug, args = {0},    delay = 1},
+        {enemy = slug, args = {0},    delay = 0.4},
+        {enemy = slug, args = {0},    delay = 0.4},
+        {enemy = slug, args = {0},    delay = 0.4},
+        {enemy = slug, args = {-200}, delay = 1},
+        {enemy = slug, args = {-200}, delay = 1},
+        {enemy = slug, args = {-200}, delay = 0.4},
+        {enemy = slug, args = {-200}, delay = 0.4},
     },
     {
-        {enemy = wraith, position = vec2(0, 340), delay = 1},
-        {enemy = wraith, position = vec2(0, 340), delay = 0.6},
-        {enemy = wraith, position = vec2(0, 340), delay = 0.6},
+        {enemy = wraith, args = {50}, delay = 1},
+        {enemy = wraith, args = {50}, delay = 1},
+        {enemy = wraith, args = {50}, delay = 1},
+        {enemy = wraith, args = {50}, delay = 1},
+    },
+    {
+        {enemy = wraith, args = {-120}, delay = 1},
+        {enemy = wraith, args = {-120}, delay = 1},
+        {enemy = wraith, args = {-120}, delay = 1},
+        {enemy = wraith, args = {-120}, delay = 1},
+    },
+    {
+        {enemy = worm, args = {-220, 1}, delay = 1},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
+        {enemy = worm, args = {-220, 1}, delay = 0.05},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
+        {enemy = worm, args = {-220, 1}, delay = 0.05},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
+        {enemy = worm, args = {-220, 1}, delay = 0.05},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
+        {enemy = worm, args = {-220, 1}, delay = 0.05},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
+        {enemy = worm, args = {-220, 1}, delay = 0.05},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
+        {enemy = worm, args = {-220, 1}, delay = 0.05},
+        {enemy = worm, args = {220, -1}, delay = 0.05},
     },
 }
 
-local enemy_frequencies = {
-    1, 1, 1,
-    2, 2, 2,
-    3, 3,
+local pattern_frequencies = {
+    1, 1,
+    2, 2,
+    3, 4,
+    5, 5, 5,
 }
 
 local vshader = [[
@@ -235,7 +433,7 @@ function init_ship()
 end
 
 local
-function spawn_explosion(state, position, size, color, color_var)
+function spawn_explosion(state, position, size, color, color_var, sound)
     local explosion =
         am.blend("add")
         ^ am.particles2d{
@@ -289,6 +487,7 @@ function end_game(state)
             return true
         end
     })
+    win.scene:action(am.play(ship_explode_buf))
 end
 
 local
@@ -350,8 +549,13 @@ function update_lasers(state)
         -- check for collision with enemies
         local hit = false
         for _, enemy in ipairs(state.enemies) do
-            if math.distance(enemy.position, pos) < enemy.radius + 5 then
+            if enemy.position.y - enemy.radius < win.top and
+                math.distance(enemy.position, pos) < enemy.radius + 5
+            then
                 enemy.hp = enemy.hp - 1
+                if enemy.hit then
+                    enemy:hit()
+                end
                 hit = true
                 break
             end
@@ -437,7 +641,12 @@ function update_enemies(state)
             spawn_explosion(state, pos, size, color, color_var)
             state.score = state.score + enemy.points
         end
-        if enemy.hp <= 0 or pos.y < win.bottom - 100 then
+        if enemy.hp <= 0 or
+            pos.y < win.bottom - 100 or
+            pos.y > win.top + 100 or
+            pos.x < win.left - 100 or
+            pos.x > win.right + 100 
+        then
             state.enemies_group:remove(enemy.node) 
             table.remove(enemies, i)
         else
@@ -454,7 +663,7 @@ function spawn_enemies(state)
     local last_t = state.last_enemy_spawn_time
     if am.frame_time > last_t + pat[phase].delay then
         -- spawn next enemy
-        local enemy = pat[phase].enemy(pat[phase].position)
+        local enemy = pat[phase].enemy(unpack(pat[phase].args))
         state.enemies_group:append(enemy.node)
         table.insert(state.enemies, enemy)
         state.last_enemy_spawn_time = am.frame_time
@@ -463,7 +672,7 @@ function spawn_enemies(state)
             -- choose new pattern
             local pool = state.pattern_pool
             if #pool == 0 then
-                table.append(pool, enemy_frequencies)
+                table.append(pool, pattern_frequencies)
             end
             local r = math.random(#pool)
             state.current_pattern = pool[r]
@@ -512,7 +721,7 @@ function start_game()
         enemies = {},
         enemies_group = enemies_group,
         explosions_group = explosions_group,
-        current_pattern = 1,
+        current_pattern = pattern_frequencies[1],
         pattern_phase = 1,
         last_enemy_spawn_time = 0,
         pattern_pool = {},
