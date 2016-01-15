bcbfde4146f40f7891a6c7012c6cbc416a2c3de8
Ian MacLarty <ian@ianmaclarty.com>
2016-01-15 16:36:13 +1100

    allow specifying tags as arguments to release_log.sh

--- scripts/release_log.sh
+++ scripts/release_log.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 set -e
 tags=`git for-each-ref --sort=taggerdate --format '%(refname)' refs/tags | sed 's/refs\/tags\///g' | grep '^v[0-9]\+\.[0-9]\+\.[0-9]\+$' | tail -n 2`
-tag1=`echo $tags | sed 's/ v[0-9]\+\.[0-9]\+\.[0-9]\+$//'`
-tag2=`echo $tags | sed 's/^v[0-9]\+\.[0-9]\+\.[0-9]\+ //'`
+if [ -z "$1" ]; then
+    tag1=`echo $tags | sed 's/ v[0-9]\+\.[0-9]\+\.[0-9]\+$//'`
+else
+    tag1=$1
+fi
+if [ -z "$2" ]; then
+    tag2=`echo $tags | sed 's/^v[0-9]\+\.[0-9]\+\.[0-9]\+ //'`
+else
+    tag2=$2
+fi
 echo Changes between $tag1 and $tag2:
 git log --oneline $tag1..$tag2
