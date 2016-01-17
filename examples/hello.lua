0302210aa68eb51118fdd25d6538b525f3f5fdcc
Ian MacLarty <ian@ianmaclarty.com>
2016-01-17 23:22:27 +1100

    remove deprecated camera2d node from hello world example

--- examples/hello.lua
+++ examples/hello.lua
@@ -1,9 +1,9 @@
-local win = am.window{title = "Untitled"}
+local win = am.window{title = "Hello"}
 
-local scene = am.camera2d(200, 150, vec2(0))
-    ^ am.scale(vec3(0, 0, 1))
-    ^ am.text("Hello, World!", vec4(0.1, 0, 0.05, 0.1))
+local scene =
+    am.scale(0)
+    ^ am.text("Hello, World!", "center", "center")
 
-scene:action(am.tween(scene"scale", 0.8, {scale = vec3(1.5, 1.5, 1)}))
+scene:action(am.tween(scene"scale", 0.8, {scale2d = vec2(4)}))
 
 win.scene = scene
