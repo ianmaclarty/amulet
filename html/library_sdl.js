d3ee03a5fd621b70133531325ba040c9e6ef5245
Ian MacLarty <ian@ianmaclarty.com>
2016-01-19 22:31:50 +1100

    always create depth+stencil bufs in editor (fixes #31)

--- html/library_sdl.js
+++ html/library_sdl.js
@@ -346,8 +346,8 @@ var LibrarySDL = {
 
       var webGLContextAttributes = {
         antialias: ((SDL.glAttributes[13 /*SDL_GL_MULTISAMPLEBUFFERS*/] != 0) && (SDL.glAttributes[14 /*SDL_GL_MULTISAMPLESAMPLES*/] > 1)),
-        depth: (SDL.glAttributes[6 /*SDL_GL_DEPTH_SIZE*/] > 0),
-        stencil: (SDL.glAttributes[7 /*SDL_GL_STENCIL_SIZE*/] > 0),
+        depth: is_in_editor || (SDL.glAttributes[6 /*SDL_GL_DEPTH_SIZE*/] > 0),
+        stencil: is_in_editor || (SDL.glAttributes[7 /*SDL_GL_STENCIL_SIZE*/] > 0),
         preserveDrawingBuffer: false,
         alpha: false,
       };
