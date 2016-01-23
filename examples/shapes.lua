e401916e572cbb9b4773f1213d992c74ed710607
Ian MacLarty <ian@ianmaclarty.com>
2016-01-23 16:43:56 +1100

    tweak shapes example

--- examples/shapes.lua
+++ examples/shapes.lua
@@ -2,11 +2,11 @@ win = am.window{title = "Shapes"}
 
 win.scene = am.group{
     -- red rectangle:
-    am.rect(-300, 200, -10, 10, vec4(1, 0, 0, 1)),
-    -- blue square:
-    am.rect(200, -200, 20, -20, vec4(0, 0, 1, 1)),
+    am.rect(-250, 180, -60, 30, vec4(1, 0, 0, 1)),
     -- yellow circle:
     am.circle(vec2(160, 120), 110, vec4(1, 1, 0, 1)),
+    -- blue triangle:
+    am.circle(vec2(160, -120), 110, vec4(0, 0, 1, 1), 3),
     -- green hexagon:
     am.circle(vec2(-160, -120), 110, vec4(0, 1, 0, 1), 6),
 }
