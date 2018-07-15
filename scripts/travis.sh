6445d1415450a4b9f59265d8bce29b6726f88fd2
Ian MacLarty <ian@ianmaclarty.com>
2018-07-15 22:42:38 +1000

    build emscripten last on osx, since it messes up the path

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -56,17 +56,6 @@ else
         make -j2 TARGET=android.release LUAVM=lua51
         make -j2 TARGET=android.release LUAVM=lua52
     else
-        # build emscripten
-        # (building on osx, because the pre-built llvm binaries don't work on linux due to incompatible glibc version)
-        EMSDK=sdk-1.38.8-64bit
-        git clone https://github.com/juj/emsdk.git emscripten
-        cd emscripten
-        ./emsdk install --enable-wasm $EMSDK
-        ./emsdk activate $EMSDK
-        source ./emsdk_env.sh
-        cd ..
-        make -j2 TARGET=html.release LUAVM=lua51
-
         # build osx
         make -j2 TARGET=osx.release     LUAVM=lua51   test
         make -j2 TARGET=osx.release     LUAVM=lua52   test
@@ -79,6 +68,17 @@ else
         scripts/build_ios.sh lua52
         rm -rf builds/ios32/lua52
         rm -rf builds/ios64/lua52
+
+        # build emscripten
+        # (building on osx, because the pre-built llvm binaries don't work on linux due to incompatible glibc version)
+        EMSDK=sdk-1.38.8-64bit
+        git clone https://github.com/juj/emsdk.git emscripten
+        cd emscripten
+        ./emsdk install --enable-wasm $EMSDK
+        ./emsdk activate $EMSDK
+        source ./emsdk_env.sh
+        cd ..
+        make -j2 TARGET=html.release LUAVM=lua51
     fi
     if [ -n "$TRAVIS_TAG" ]; then
         scripts/upload_builds.js $TRAVIS_TAG
