578fc31056a69f17cc0d442dcd046b705e444efc
Ian MacLarty <ian@ianmaclarty.com>
2018-09-10 09:26:36 +1000

    try to fix appveyour missing zip command

--- scripts/appveyor.bat
+++ scripts/appveyor.bat
@@ -1,38 +1,42 @@
 call "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
 set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;"C:\Program Files (x86)\Inno Setup 5";%PATH%
-
-if "%APPVEYOR_REPO_TAG_NAME:~-15%" == "-distro-trigger" goto builddistro
-
-make TARGET=msvc32.release LUAVM=lua51 test
-make TARGET=msvc32.release LUAVM=lua52 test
-make TARGET=msvc32.release LUAVM=luajit test
-if %errorlevel% neq 0 exit /b %errorlevel%
-if defined APPVEYOR_REPO_TAG_NAME (node scripts\upload_builds.js %APPVEYOR_REPO_TAG_NAME%)
-if %errorlevel% neq 0 exit /b %errorlevel%
-goto end
-
-:builddistro
-set TAG=%APPVEYOR_REPO_TAG_NAME:~0,-15%
-choco install -y InnoSetup
-curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-darwin.zip
-curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-win32.zip
-curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-linux.zip
-unzip builds-darwin.zip
-unzip builds-win32.zip
-unzip builds-linux.zip
-mkdir scripts\installer-payload
-cp -r builds scripts/installer-payload/
-cp -r builds/msvc32/luajit/release/bin/* scripts/installer-payload/
-mv scripts/installer-payload/amulet.exe scripts/installer-payload/amulet-window.exe
-mv scripts/installer-payload/amulet-console.exe scripts/installer-payload/amulet.exe
-cp -r examples scripts/installer-payload/
-cp scripts/icon.ico scripts/installer-payload/
-cp scripts/amulet_env.bat scripts/installer-payload/
-iscc /DVERSION=%TAG% /O. /Famulet-%TAG%-windows scripts\installer.iss
-if %errorlevel% neq 0 exit /b %errorlevel%
-mv scripts/installer-payload amulet-%TAG%
-zip -r amulet-%TAG%-windows.zip amulet-%TAG%
-node scripts\upload_distros.js %TAG% amulet-%TAG%-windows.exe amulet-%TAG%-windows.zip
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-:end
+zip -r test.zip Makefile
+find /c/msys64 -type f -name "zip.exe"
+find /c/msys64 -type f -name "zip.*"
+echo DONE
+REM 
+REM if "%APPVEYOR_REPO_TAG_NAME:~-15%" == "-distro-trigger" goto builddistro
+REM 
+REM make TARGET=msvc32.release LUAVM=lua51 test
+REM make TARGET=msvc32.release LUAVM=lua52 test
+REM make TARGET=msvc32.release LUAVM=luajit test
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM if defined APPVEYOR_REPO_TAG_NAME (node scripts\upload_builds.js %APPVEYOR_REPO_TAG_NAME%)
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM goto end
+REM 
+REM :builddistro
+REM set TAG=%APPVEYOR_REPO_TAG_NAME:~0,-15%
+REM choco install -y InnoSetup
+REM curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-darwin.zip
+REM curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-win32.zip
+REM curl -L -O https://github.com/ianmaclarty/amulet/releases/download/%TAG%/builds-linux.zip
+REM unzip builds-darwin.zip
+REM unzip builds-win32.zip
+REM unzip builds-linux.zip
+REM mkdir scripts\installer-payload
+REM cp -r builds scripts/installer-payload/
+REM cp -r builds/msvc32/luajit/release/bin/* scripts/installer-payload/
+REM mv scripts/installer-payload/amulet.exe scripts/installer-payload/amulet-window.exe
+REM mv scripts/installer-payload/amulet-console.exe scripts/installer-payload/amulet.exe
+REM cp -r examples scripts/installer-payload/
+REM cp scripts/icon.ico scripts/installer-payload/
+REM cp scripts/amulet_env.bat scripts/installer-payload/
+REM iscc /DVERSION=%TAG% /O. /Famulet-%TAG%-windows scripts\installer.iss
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM mv scripts/installer-payload amulet-%TAG%
+REM zip -r amulet-%TAG%-windows.zip amulet-%TAG%
+REM node scripts\upload_distros.js %TAG% amulet-%TAG%-windows.exe amulet-%TAG%-windows.zip
+REM if %errorlevel% neq 0 exit /b %errorlevel%
+REM 
+REM :end
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,87 +1,87 @@
 #!/bin/bash
-set -e
-if [[ "$TRAVIS_TAG" == *-distro-trigger ]]; then
-    TAG=${TRAVIS_TAG%-distro-trigger}
-    VERSION=`echo $TAG | tr -d v`
-    curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-darwin.zip
-    curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-win32.zip
-    curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-linux.zip
-    unzip builds-darwin.zip
-    unzip builds-win32.zip
-    unzip builds-linux.zip
-    mkdir amulet-${TAG}
-    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-        cp -r builds/linux64/luajit/release/bin/* amulet-${TAG}/
-        cp -r builds/linux32/luajit/release/bin/amulet amulet-${TAG}/amulet.i686
-        chmod a+x amulet-${TAG}/amulet
-        chmod a+x amulet-${TAG}/amulet.i686
-    else
-        cp -r builds/osx/luajit/release/bin/* amulet-${TAG}/
-        chmod a+x amulet-${TAG}/amulet
-    fi
-    mv builds amulet-${TAG}/
-    zip -r amulet-${TAG}-${TRAVIS_OS_NAME}.zip amulet-${TAG}
-    FILE_LIST=amulet-${TAG}-${TRAVIS_OS_NAME}.zip
-    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-        mkdir -p pkg_payload/usr/local/bin
-        mkdir -p pkg_payload/usr/local/share/amulet
-        mv amulet-${TAG}/builds pkg_payload/usr/local/share/amulet/
-        mv amulet-${TAG}/amulet pkg_payload/usr/local/bin/
-        pkgbuild --identifier xyz.amulet.pkg --version $VERSION --root pkg_payload/ --install-location / amulet-${TAG}-${TRAVIS_OS_NAME}.pkg
-        FILE_LIST="$FILE_LIST amulet-${TAG}-${TRAVIS_OS_NAME}.pkg"
-    fi
-    scripts/upload_distros.js $TAG $FILE_LIST
-else
-    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-        # build linux
-        make -j2 TARGET=linux32.release LUAVM=lua51   test
-        make -j2 TARGET=linux32.release LUAVM=lua52   test
-        make -j2 TARGET=linux32.release LUAVM=luajit  test
-        make -j2 TARGET=linux64.release LUAVM=lua51   test
-        make -j2 TARGET=linux64.release LUAVM=lua52   test
-        make -j2 TARGET=linux64.release LUAVM=luajit  test
-        make -j2 TARGET=mingw32.release LUAVM=lua51
-        make -j2 TARGET=mingw32.release LUAVM=lua52
-        scripts/gen_linux_universal.sh
-
-        # build android
-        NDK=android-ndk-r14b
-        echo downloading $NDK...
-        curl -s -L https://dl.google.com/android/repository/$NDK-linux-x86_64.zip -o android-ndk.zip
-        echo unzipping $NDK...
-        unzip android-ndk.zip > /dev/null
-        export NDK_HOME=`pwd`/$NDK
-        export NDK_HOST=linux-x86_64
-        export NDK_ANDROID_VER=16
-        make -j2 TARGET=android.release LUAVM=lua51
-        make -j2 TARGET=android.release LUAVM=lua52
-    else
-        # build osx
-        make -j2 TARGET=osx.release     LUAVM=lua51   test
-        make -j2 TARGET=osx.release     LUAVM=lua52   test
-        make -j2 TARGET=osx.release     LUAVM=luajit  test
-
-        # build ios
-        scripts/build_ios.sh lua51
-        rm -rf builds/ios32/lua51
-        rm -rf builds/ios64/lua51
-        scripts/build_ios.sh lua52
-        rm -rf builds/ios32/lua52
-        rm -rf builds/ios64/lua52
-
-        # build emscripten
-        # (building on osx, because the pre-built llvm binaries don't work on linux due to incompatible glibc version)
-        EMSDK=sdk-1.38.8-64bit
-        git clone https://github.com/juj/emsdk.git emscripten
-        cd emscripten
-        ./emsdk install --enable-wasm $EMSDK
-        ./emsdk activate $EMSDK
-        source ./emsdk_env.sh
-        cd ..
-        make -j2 TARGET=html.release LUAVM=lua51
-    fi
-    if [ -n "$TRAVIS_TAG" ]; then
-        scripts/upload_builds.js $TRAVIS_TAG
-    fi
-fi
-scripts/update_site.sh
+#set -e
+#if [[ "$TRAVIS_TAG" == *-distro-trigger ]]; then
+#    TAG=${TRAVIS_TAG%-distro-trigger}
+#    VERSION=`echo $TAG | tr -d v`
+#    curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-darwin.zip
+#    curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-win32.zip
+#    curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-linux.zip
+#    unzip builds-darwin.zip
+#    unzip builds-win32.zip
+#    unzip builds-linux.zip
+#    mkdir amulet-${TAG}
+#    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+#        cp -r builds/linux64/luajit/release/bin/* amulet-${TAG}/
+#        cp -r builds/linux32/luajit/release/bin/amulet amulet-${TAG}/amulet.i686
+#        chmod a+x amulet-${TAG}/amulet
+#        chmod a+x amulet-${TAG}/amulet.i686
+#    else
+#        cp -r builds/osx/luajit/release/bin/* amulet-${TAG}/
+#        chmod a+x amulet-${TAG}/amulet
+#    fi
+#    mv builds amulet-${TAG}/
+#    zip -r amulet-${TAG}-${TRAVIS_OS_NAME}.zip amulet-${TAG}
+#    FILE_LIST=amulet-${TAG}-${TRAVIS_OS_NAME}.zip
+#    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+#        mkdir -p pkg_payload/usr/local/bin
+#        mkdir -p pkg_payload/usr/local/share/amulet
+#        mv amulet-${TAG}/builds pkg_payload/usr/local/share/amulet/
+#        mv amulet-${TAG}/amulet pkg_payload/usr/local/bin/
+#        pkgbuild --identifier xyz.amulet.pkg --version $VERSION --root pkg_payload/ --install-location / amulet-${TAG}-${TRAVIS_OS_NAME}.pkg
+#        FILE_LIST="$FILE_LIST amulet-${TAG}-${TRAVIS_OS_NAME}.pkg"
+#    fi
+#    scripts/upload_distros.js $TAG $FILE_LIST
+#else
+#    if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+#        # build linux
+#        make -j2 TARGET=linux32.release LUAVM=lua51   test
+#        make -j2 TARGET=linux32.release LUAVM=lua52   test
+#        make -j2 TARGET=linux32.release LUAVM=luajit  test
+#        make -j2 TARGET=linux64.release LUAVM=lua51   test
+#        make -j2 TARGET=linux64.release LUAVM=lua52   test
+#        make -j2 TARGET=linux64.release LUAVM=luajit  test
+#        make -j2 TARGET=mingw32.release LUAVM=lua51
+#        make -j2 TARGET=mingw32.release LUAVM=lua52
+#        scripts/gen_linux_universal.sh
+#
+#        # build android
+#        NDK=android-ndk-r14b
+#        echo downloading $NDK...
+#        curl -s -L https://dl.google.com/android/repository/$NDK-linux-x86_64.zip -o android-ndk.zip
+#        echo unzipping $NDK...
+#        unzip android-ndk.zip > /dev/null
+#        export NDK_HOME=`pwd`/$NDK
+#        export NDK_HOST=linux-x86_64
+#        export NDK_ANDROID_VER=16
+#        make -j2 TARGET=android.release LUAVM=lua51
+#        make -j2 TARGET=android.release LUAVM=lua52
+#    else
+#        # build osx
+#        make -j2 TARGET=osx.release     LUAVM=lua51   test
+#        make -j2 TARGET=osx.release     LUAVM=lua52   test
+#        make -j2 TARGET=osx.release     LUAVM=luajit  test
+#
+#        # build ios
+#        scripts/build_ios.sh lua51
+#        rm -rf builds/ios32/lua51
+#        rm -rf builds/ios64/lua51
+#        scripts/build_ios.sh lua52
+#        rm -rf builds/ios32/lua52
+#        rm -rf builds/ios64/lua52
+#
+#        # build emscripten
+#        # (building on osx, because the pre-built llvm binaries don't work on linux due to incompatible glibc version)
+#        EMSDK=sdk-1.38.8-64bit
+#        git clone https://github.com/juj/emsdk.git emscripten
+#        cd emscripten
+#        ./emsdk install --enable-wasm $EMSDK
+#        ./emsdk activate $EMSDK
+#        source ./emsdk_env.sh
+#        cd ..
+#        make -j2 TARGET=html.release LUAVM=lua51
+#    fi
+#    if [ -n "$TRAVIS_TAG" ]; then
+#        scripts/upload_builds.js $TRAVIS_TAG
+#    fi
+#fi
+#scripts/update_site.sh
