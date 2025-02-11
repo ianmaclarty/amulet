4737a47005db38704f1a0143ee3e11d3212f94e8
Ian MacLarty <ian.maclarty@gmail.com>
2025-02-11 15:39:13 +1100

    github workflow fixes

--- scripts/github_build_windows.bat
+++ scripts/github_build_windows.bat
@@ -1,6 +1,3 @@
-pacman -S --noconfirm zip
-
-call "%programfiles(x86)%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 make STRICT=1 TARGET=msvc64.release LUAVM=luajit test
 make STRICT=1 TARGET=msvc64.release LUAVM=lua51 test
