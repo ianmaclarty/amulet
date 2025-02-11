90eee3ffe21dedebb6ec8f01f97f793cd1e58440
Ian MacLarty <ian.maclarty@gmail.com>
2025-02-11 22:57:44 +1100

    more github workflow fixes

--- scripts/github_build_distros.sh
+++ scripts/github_build_distros.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 set -e
-TAG=${TRAVIS_TAG%-distro-trigger}
-VERSION=`echo $TAG | tr -d v`
-curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-darwin.zip
-curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-win32.zip
-curl -L -O https://github.com/ianmaclarty/amulet/releases/download/${TAG}/builds-linux.zip
-unzip builds-darwin.zip
-unzip builds-win32.zip
-unzip builds-linux.zip
-mkdir amulet-${TAG}
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    cp -r builds/linux64/luajit/release/bin/* amulet-${TAG}/
-    cp -r builds/linux32/luajit/release/bin/amulet amulet-${TAG}/amulet.i686
-    chmod a+x amulet-${TAG}/amulet
-    chmod a+x amulet-${TAG}/amulet.i686
-else
-    cp -r builds/osx/luajit/release/bin/amulet amulet-${TAG}/
-    chmod a+x amulet-${TAG}/amulet
-fi
-mv templates amulet-${TAG}/
-mv builds amulet-${TAG}/
-zip -r amulet-${TAG}-${TRAVIS_OS_NAME}.zip amulet-${TAG}
-FILE_LIST=amulet-${TAG}-${TRAVIS_OS_NAME}.zip
+
+# linux
+mkdir amulet-${GITHUB_TAG}
+cp -r builds/linux64/lua51/release/bin/* amulet-${GITHUB_TAG}/
+chmod a+x amulet-${GITHUB_TAG}/amulet
+cp -r builds amulet-${GITHUB_TAG}/
+cp -r templates amulet-${GITHUB_TAG}/
+zip -r amulet-${GITHUB_TAG}-linux.zip amulet-${GITHUB_TAG}
+rm -rf amulet-${GITHUB_TAG}
+
+# macos
+mkdir amulet-${GITHUB_TAG}
+cp -r builds/osx/lua51/release/bin/amulet amulet-${GITHUB_TAG}/
+chmod a+x amulet-${GITHUB_TAG}/amulet
+cp -r builds amulet-${GITHUB_TAG}/
+cp -r templates amulet-${GITHUB_TAG}/
+zip -r amulet-${GITHUB_TAG}-macos.zip amulet-${GITHUB_TAG}
+rm -rf amulet-${GITHUB_TAG}
+
+# windows
+mkdir amulet-${GITHUB_TAG}
+cp -r builds/msvc64/lua51/release/bin/* amulet-${GITHUB_TAG}/
+mv amulet-${GITHUB_TAG}/amulet.exe amulet-${GITHUB_TAG}/amulet-window.exe
+mv amulet-${GITHUB_TAG}/amulet-console.exe amulet-${GITHUB_TAG}/amulet.exe
+cp -r builds amulet-${GITHUB_TAG}/
+cp -r templates amulet-${GITHUB_TAG}/
+zip -r amulet-${GITHUB_TAG}-windows.zip amulet-${GITHUB_TAG}
+rm -rf amulet-${GITHUB_TAG}
